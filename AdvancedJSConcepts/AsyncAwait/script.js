6028fdfe5050d8ec50af67ebea55b6bfaa91d004
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-14 23:14:49 +0530

    modified the script.js file

--- AdvancedJSConcepts/AsyncAwait/script.js
+++ AdvancedJSConcepts/AsyncAwait/script.js
@@ -4,3 +4,35 @@ async function getUserData() {
     const data = await response.json();
     return data;    
 }
+
+// Error Handling
+async function fetchData() {
+    try{
+        const data = await fetch('https://api.github.com/users');
+        return data.json();
+    } catch (error) {
+        console.log('Something went wrong',error);
+        return null;
+    }
+}
+
+// Running tasks in parallel
+
+// sequential (slower)
+const userData = await fetchUser();
+const postsData = await fetchPosts();
+
+// parallel (faster)
+const [userData, postsData] = await Promise.all([fetchUser(), fetchPosts()]);
+
+// API Calls
+async function getUser(id) {
+    const user = await fetch(`https://api.github.com/users/${id}`);
+    return user.json();
+}
+
+// Database Operations
+async function saveUser(userData) {
+    const result = await db.collection('users').insertOne(userData);
+    return result;
+}
