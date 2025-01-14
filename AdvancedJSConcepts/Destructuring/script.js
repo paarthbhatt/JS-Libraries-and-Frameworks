3b607d1143c9b3162e93330cc28bf8dcf4d9f5e1
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-14 19:47:29 +0530

    modified script.js

--- AdvancedJSConcepts/Destructuring/script.js
+++ AdvancedJSConcepts/Destructuring/script.js
@@ -8,3 +8,41 @@ const person = {
     city: 'New York', 
     occupation: 'Software Engineer',
 };
+
+// Old Way
+
+const firstName = person.firstName;
+const lastName = person.lastName;
+
+// Destructuring Way
+
+const {firstName, lastName} = person;
+console.log(firstName, lastName);
+
+// Renaming variables while destructuring
+
+const {firstName:fName, lastName: lName} = person;
+console.log(fName, lName);
+
+// Default Values
+const {age, country = 'USA'} = person;
+console.log(age, country);
+
+
+// NESTED OD
+
+const user = {
+    id: 1,
+    name: 'John',
+    age: 30,
+    education: {
+        degree: 'Masters',
+        school: 'MIT'
+    }
+};
+
+// Destructuring Nested Objects
+const {education: {degree, school}} = user;
+console.log(degree, school);
+
+// ARRAY DESTRUCTURING
