311e25c5edeec91f549b6b556f7d2a1608307ea8
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-14 20:08:17 +0530

    modified script.js

--- AdvancedJSConcepts/Destructuring/script.js
+++ AdvancedJSConcepts/Destructuring/script.js
@@ -46,3 +46,50 @@ const {education: {degree, school}} = user;
 console.log(degree, school);
 
 // ARRAY DESTRUCTURING
+
+const numbers = [1, 2, 3, 4, 5];
+
+// BASIC AD
+const [first, second] = numbers;
+console.log(first, second);
+const [first, , third] = numbers;
+console.log(first, third);
+
+// Rest Operator with Arrays
+const [first, second, ...rest] = numbers;
+console.log(first, second, rest);
+
+
+ // SWAPPING VARIABLES
+
+ let a = 5;
+ let b = 10;
+
+ // Old Way
+ let temp = a;
+    a = b;
+    b = temp;
+
+// Destructuring Way
+[a, b] = [b, a];
+console.log(a, b);
+
+
+// FUNCTION PARAMETER DESTRUCTURING
+
+function printUserInfo({name, age, city='Unknown'}) {
+    console.log(`Name: ${name}, Age: ${age}, City: ${city}`);
+}
+const user = {
+    name: 'John',
+    age: 30
+    country: 'USA'
+};
+printUserInfo(user);
+
+// Array Parameter Destructuring
+
+function getFirstTwo([first, second]) {
+    return 'First: ${first}, Second: ${second}';
+}
+console.log(getFirstTwo([1,2,3]))
