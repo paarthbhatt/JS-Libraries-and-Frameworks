e22cfeea9de6e12e61aef91abf0e1029fbe25783
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-16 18:01:46 +0530

    made changes and added file for nextjs webpage

---
+++ ReactJS/reactproject/app/Courses/page.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div>
+      Courses
+    </div>
+  )
+}
+
+export default Courses
--- ReactJS/reactproject/app/page.js
+++ ReactJS/reactproject/app/page.js
@@ -1,17 +1,13 @@
 "use client"
 import React , { useState } from 'react'
-import Header from "@/Components/header"
-
 const page = () => {
-  const [user, setUser] = useState("Parth Bhatt")
-  const [editor, setEditor] = useState("GitHub")
   return (
-    <>
-      <Header user={user} edit={editor}/>
-      {user}
-    </>
+    <div>
+      <a href='about'>About</a>
+      <a href=''>Product</a>
+      <a href=''>Course</a>
+    </div>
   )
 }
 
 export default page
-  
