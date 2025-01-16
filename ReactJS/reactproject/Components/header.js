a92b210979d5c56cbcb7c830592c5ac63a4308d9
Parth Bhatt <paarthbhatt37@gmail.com>
2025-01-16 16:25:00 +0530

    used props

--- ReactJS/reactproject/Components/header.js
+++ ReactJS/reactproject/Components/header.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
-const header = () => {
+const header = (props) => {
   return (
     
     <div className='h-16 px-5 bg-green-500 flex items-center justify-between'>
-        <h2>Company Logo</h2>
+        <h2>{props.user}</h2>
         <div className='flex gap-8'>
             <h4>About Us</h4>
             <h4>Careers</h4>
             <h4>Blogs</h4>
-            <h4>Accounts</h4>
+            <h4>{props.edit}</h4>
         </div>
-    </div>
-    
+    </div>    
   )
 }
 
--- ReactJS/reactproject/app/page.js
+++ ReactJS/reactproject/app/page.js
@@ -4,12 +4,14 @@ import Header from "@/Components/header"
 
 const page = () => {
   const [user, setUser] = useState("Parth Bhatt")
+  const [editor, setEditor] = useState("GitHub")
   return (
     <>
-      <Header/>
+      <Header user={user} edit={editor}/>
       {user}
     </>
   )
 }
 
 export default page
+  
